f51eb2e0846660a90d8688ebc771bb3f76bfed1e
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-09-28 21:47:50 +0600

    form target value got

---
+++ src/__form__interactions.js
@@ -0,0 +1,4 @@
+/**
+ * 1. forn ==> e.target.[name of the input field].value
+ * 
+*/
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,13 +1,20 @@
 
 const Register = () => {
+    const handleRegister = e => {
+        e.preventDefault();
+        console.log('form submitting');
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
     return (
-        <div className="border">
+        <div className="">
             <div className="mx-auto md:w-1/2">
                 <h2 className="text-3xl mb-8">Please Register</h2>
-                <form>
-                    <input className="mb-4 w-3/4 py-2 px-4" type="email" name="email" id="" />
+                <form onSubmit={handleRegister}>
+                    <input className="mb-4 w-3/4 py-2 px-4" type="email" name="email" placeholder="Email Address" id="" />
                     <br />
-                    <input className="mb-4 w-3/4 py-2 px-4" type="password" name="" id="" />
+                    <input className="mb-4 w-3/4 py-2 px-4" type="password" name="password" placeholder="Password" id="" />
                     <br />
                     <input className="btn btn-secondary mb-4 w-3/4" type="submit" value="Register" />
                 </form>

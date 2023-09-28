74bd027712f34e71964a9272f9013e2c2ad0a588
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-09-28 22:06:13 +0600

    DaisyUI Hero Form Data got

--- src/components/HeroRegister/HeroRegister.jsx
+++ src/components/HeroRegister/HeroRegister.jsx
@@ -3,6 +3,9 @@ const HeroRegister = () => {
     const handleRegister = e => {
         e.preventDefault();
         console.log('form submitted');
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
     }
     return (
         <div>
@@ -19,13 +22,13 @@ const HeroRegister = () => {
                                     <label className="label">
                                         <span className="label-text">Email</span>
                                     </label>
-                                    <input type="email" placeholder="email" className="input input-bordered" />
+                                    <input type="email" placeholder="email" name="email" className="input input-bordered" />
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type="password" placeholder="password" className="input input-bordered" />
+                                    <input type="password" placeholder="password" name="password" className="input input-bordered" />
                                     <label className="label">
                                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                     </label>

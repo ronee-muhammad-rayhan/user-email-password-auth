534e973800b378bfa95744d2c2ad066f35bf5531
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-09-28 21:30:39 +0600

    form added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,17 @@
 
 const Register = () => {
     return (
-        <div>
-            <h2 className="text-3xl">Please Register</h2>
+        <div className="border">
+            <div className="mx-auto md:w-1/2">
+                <h2 className="text-3xl mb-8">Please Register</h2>
+                <form>
+                    <input className="mb-4 w-3/4 py-2 px-4" type="email" name="email" id="" />
+                    <br />
+                    <input className="mb-4 w-3/4 py-2 px-4" type="password" name="" id="" />
+                    <br />
+                    <input className="btn btn-secondary mb-4 w-3/4" type="submit" value="Register" />
+                </form>
+            </div>
         </div>
     );
 };
